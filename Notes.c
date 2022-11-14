c069f537e61784dffeb408fddab524a0b1a0cf52
Zoro <118174678+Mugiwara312@users.noreply.github.com>
2022-11-14 16:15:50 +0530

    Delete Notes.c

--- Notes.c
+++
@@ -1,9 +0,0 @@
-/*                                                                          C Notes
-<Print Function>*/
-
-#include <stdio.h>
-int main()
-{
-    printf("Hello World");
-    return 0;
-}
